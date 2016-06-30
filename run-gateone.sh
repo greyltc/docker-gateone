2f3a8578c889a581c69c6a510b6edec6ab0e935b
Grey Christoforo <grey@christoforo.net>
2016-06-30 19:49:15 +0100

    Update run-gateone.sh

--- run-gateone.sh
+++ run-gateone.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-gateone --port 7889&
+
+# generate settings
+gateone --help
+
+# fix default port
+sed -i 's|"port": 443,|"port": 10443,|g' /etc/gateone/conf.d/10server.conf
+
+gateone &
