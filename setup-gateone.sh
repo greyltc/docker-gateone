b5bfc6f048d9d87aaf8be72553f3207a9de21fc1
Grey Christoforo <grey@christoforo.net>
2016-06-30 19:48:55 +0100

    Update setup-gateone.sh

--- setup-gateone.sh
+++ setup-gateone.sh
@@ -2,11 +2,5 @@
 
 su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm dtach mutagen python2-imaging python2-pyopenssl gateone-git'
 
-# generate settings
-gateone --help
-
-# fix default port
-sed -i 's|"port": 443,|"port": 10443,|g' /etc/gateone/conf.d/10server.conf
-
 # fix the python issue
 sed -i 's,#!/usr/bin/env python,#!/usr/bin/env python2,g' /usr/lib/python2.7/site-packages/gateone/applications/terminal/plugins/ssh/scripts/ssh_connect.py
