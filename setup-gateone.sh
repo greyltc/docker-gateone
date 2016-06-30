b2acaf01a00e795b8fa40895ff0cf737c95250da
Grey Christoforo <grey@christoforo.net>
2016-06-30 18:46:54 +0100

    Update setup-gateone.sh

--- setup-gateone.sh
+++ setup-gateone.sh
@@ -3,4 +3,3 @@
 su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm dtach mutagen python2-imaging python2-pyopenssl gateone-git'
 # fix the python issue
 sed -i 's,#!/usr/bin/env python,#!/usr/bin/env python2,g' /usr/lib/python2.7/site-packages/gateone/applications/terminal/plugins/ssh/scripts/ssh_connect.py
-sed -i 's,#!/usr/bin/env python,#!/usr/bin/env python2,g' /usr/lib/python2.7/site-packages/gateone/applications/terminal/plugins/ssh/scripts/timeout.py
