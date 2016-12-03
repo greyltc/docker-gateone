01219a79c4a14f6157fddfc975ad4427853d65a2
Grey Christoforo <grey@christoforo.net>
2016-12-03 18:32:38 +0000

    need locomotive!

--- setup-gateone.sh
+++ setup-gateone.sh
@@ -2,7 +2,7 @@
 
 pacman -S sl vim
 
-su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm dtach mutagen python2-imaging python2-pyopenssl gateone-git'
+su docker -c 'pacaur -S --noprogressbar --needed --noedit --noconfirm dtach mutagen python2-imaging python2-pyopenssl gateone-git sl'
 
 # fix the python issue
 sed -i 's,#!/usr/bin/env python,#!/usr/bin/env python2,g' /usr/lib/python2.7/site-packages/gateone/applications/terminal/plugins/ssh/scripts/ssh_connect.py
